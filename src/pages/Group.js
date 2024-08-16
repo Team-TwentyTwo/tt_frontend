dfefda67507293c4a127bd26b6d04a5f54688d3a
sumhillj <sumhillj@gmail.com>
2024-08-16 19:48:45 +0900

    Build : App컴포넌트 및 디렉토리 구조 수정 외

--- src/pages/Group.js
+++ src/pages/Group.js
@@ -1,6 +1,24 @@
+import Header from "../components/Header";
+import mbutton from "../assets/group_makebtn_m.svg";
+import styles from './Group.module.css';
+
 function Group() {
+  const handleButtonClick = () => {
+    console.log("그룹 만들기 요청");
+  };
+
   return (
     <>
+      <Header>
+        <div className={styles.buttonContainer}>
+          <img 
+            src={mbutton} 
+            alt="그룹 만들기 버튼" 
+            className={styles.makeButton} 
+            onClick={handleButtonClick} 
+          />
+        </div>
+      </Header>
     </>
   );
 }

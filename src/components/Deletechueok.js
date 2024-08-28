52d7c75889407ed9a6f5b6436879d3568d12b1db
CHJIYEON <jy0311@yonsei.ac.kr>
2024-08-28 18:26:35 +0900

    .

--- src/components/Deletechueok.js
+++ src/components/Deletechueok.js
@@ -5,16 +5,16 @@ import x from '../assets/icon_x'
 function Deletechueok() {
     return (
       <>
-      <h1>추억 삭제</h1>
-      <h2>삭제 권한 인증</h2>
-      <div>
-        <h3>비밀번호를 입력해 주세요</h3>
+        <h1>추억 삭제</h1>
+        <h2>삭제 권한 인증</h2>
+        <div>
+          <h3>비밀번호를 입력해 주세요</h3>
+          
+        </div>
+        <div>
+          <img src={deletebutton} alt="삭제하기버튼" />
+        </div>
         
-      </div>
-      <div>
-      <img src={deletebutton} alt="삭제하기버튼" />
-      </div>
-      
 
       
       </>
